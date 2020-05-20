9ee6b7ffaf393facd89715e904a5340a3dda7cc7
Hemen Kapadia <hemen.kapadia@gmail.com>
2020-05-19 19:36:34 -0700

    Updating Setup.py and external version file

--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
-import setuptools
+from setuptools import setup, find_packages
+
+# read version
+about_info = {}
+with open("version.txt", "r") as v:
+    exec(v.read(), about_info)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-setuptools.setup(
-    name="craft-text-detection",
-    version="0.0.1",
-    author="Clova AI Research, NAVER Corp., Ashish Jha",
-    author_email="youngmin.baek@navercorp.com, arj7192@gmail.com",
-    description="Official implementation of Character Region Awareness for Text Detection (CRAFT)",
+setup(
+    name="h2o_craft",
+    version=about_info["version"],
+    description="H2O.ai port of Pytorch implementation of CRAFT text detector",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/clovaai/CRAFT-pytorch",
-    packages=['craft'],
+    url="https://www.h2o.ai",
+    author="H2O.ai",
+    author_email="team@h2o.ai",
+    license="MIT License",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
     ],
-    install_requires=required,
-    python_requires='>=3.6',
+    keywords="",
+    packages=['craft'],
+    python_requires="~=3.6",
+    install_requires=[],
+    dependency_links=[],
+    # package_data={},
+    # data_files=[],
+    # entry_points={},
 )
