87d7a2a49ea9b603e36e4d74468788c1492a183c
Prathamesh Zade <prathmeshpaware9090@gmail.com>
2024-02-12 14:11:36 +0530

    defined model_urls

--- basenet/vgg16_bn.py
+++ basenet/vgg16_bn.py
@@ -4,7 +4,10 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 from torchvision import models
-from torchvision.models.vgg import model_urls
+
+model_urls = {
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+}
 
 def init_weights(modules):
     for m in modules:
